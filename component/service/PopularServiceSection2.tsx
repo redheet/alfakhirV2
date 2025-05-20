acf97d9d1c27b7c957b62720febbb21070ab5406
redheet <naufalzidane32@gmail.com>
2025-05-20 13:53:55 +0700

    remove id tag

--- component/service/PopularServiceSection2.tsx
+++ component/service/PopularServiceSection2.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import ServiceSlider from "../slider/ServiceSlider";
 import { ServiceType } from "@/types";
+import Script from "next/script";
 interface Props {
   services: ServiceType[];
 }
 const PopularServiceSection2 = ({ services }: Props) => {
   return (
-    <div className="tf__popular_services mt_100 pt_95 pb_100" id="program">
+    <div className="tf__popular_services mt_100 pt_95 pb_100" >
       <div className="container">
         <div className="row">
           <div className="col-xl-6 col-md-8 col-lg-6 m-auto wow fadeInUp">
             <div className="tf__heading_area mb_40">
-              <h5>Program</h5>
+              <h5 >Program</h5>
               <h2>Program Kami</h2>
             </div>
           </div>
